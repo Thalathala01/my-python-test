0c58b431af6fbcef7c23cfc9e02b6cb1f2771a66
Thalathala01 <thalapathy1804@gmail.com>
2025-06-11 19:05:06 +0530

    fix: corrected multiply function to use multiplication

---
+++ basic_math.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    # Bug: multiplies incorrectly by adding instead
+    return a + b  # should be a * b
+
+def fixed_multiply(a, b):
+    # Fixed to multiply properly
+    return a * b
